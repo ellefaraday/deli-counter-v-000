11ed6ab9f54d79ac1d0d8394d4333c0e82469df2
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 17:12:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line (kats_deli)
+  if kats_deli == []
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{kats_deli.collect.with_index {|name, index| index, ".", name, " "}}"
+  end
+end
