933af2cc72e4fe792e080219a40ac52c42a44196
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 17:12:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,7 @@ def line (kats_deli)
     puts "The line is currently: #{kats_deli.collect.with_index {|name, index| index, ".", name, " "}}"
   end
 end
+
+kats_deli = ["Elle", "Margot"]
+
+line(kats_deli)
